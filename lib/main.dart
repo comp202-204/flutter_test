cc6b38130d1ec7969fce82d186b78458f290108e
Oblamov <ahmet_nt@outlook.com>
2024-03-07 02:03:40 +0300

    Second try toocommit

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,18 @@ class myApp extends StatelessWidget {
 
   }
 }
-
-
 class TextLearnView extends StatelessWidget{
   const TextLearnView({Key? key })  : super(key: key);
 
 
   Widget build (BuildContext context){
 
-    return const Scaffold(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          child: Icon(Icons.arrow_circle_down_sharp),
+
+      ),
       body: Center(child:
       Text(
           'Eerenss'
